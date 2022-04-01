a870f43839cb3b095ea4dd3da7b7a842ebc1b727
mmerveon <45422341+mmerveon@users.noreply.github.com>
2022-04-01 10:54:11 +0200

    inhoud kopjes aangepast

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ import epicLine from './pictures/epicLineFinished.png'
 class App extends React.Component {
 
   static defaultProps = {
-    isOpened: true
+    isOpened: true,
+    isOpened2: true,
+    isOpened3: true,
+    isOpened4: true,
+    isOpened5: true
   };
 
   constructor(props) {
@@ -93,9 +97,9 @@ class App extends React.Component {
 
     const {isOpened} = this.state;
     const {isOpened2} = this.state;
-    // const {isOpened3} = this.state;
-    // const {isOpened4} = this.state;
-    // const {isOpened5} = this.state;
+    const {isOpened3} = this.state;
+    const {isOpened4} = this.state;
+    const {isOpened5} = this.state;
     
     return (
       <form onSubmit={this.handleSubmit}>
@@ -111,8 +115,10 @@ class App extends React.Component {
         <Collapse isOpened={isOpened}>
         
           <div className="vraag">
-            <h>Vraag 1</h>
-            <p>dit is vraag 1</p>
+            <b><p>MVO is een veelomvattend begrip. Bepaal de belangrijkste MVO-thema’s voor jouw bedrijf of organisatie, als eerste belangrijke stap.</p></b>
+            <h>MVO IS EEN BREED BEGRIP</h>
+            <p>Bepaal de belangrijkste MVO-thema’s voor jouw bedrijf of organisatie, als eerste belangrijke stap. De kernthema’s van MVO zijn uitgewerkt in ISO 2600. 
+               Ook de Sustainable Development Goals (SDG’s) van de Verenigde Naties geven het brede kader van duurzaamheid aan. </p>
             <div name="gender" onChange={this.handleInputChange}>
               <input type="radio" value="MALE" name="gender"/> Male
               <input type="radio" value="FEMALE" name="gender"/> Female
@@ -127,11 +133,11 @@ class App extends React.Component {
             className="checkboxinvis"
             type="checkbox"
             id='checkbox2'
-            checked={isOpened}
-            onChange={({target: {checked}}) => this.setState({isOpened: checked})} />
+            checked={isOpened2}
+            onChange={({target: {checked}}) => this.setState({isOpened2: checked})} />
             <label for="checkbox2"><img src={collapse} className="imgButton" /></label>
             <h3>MVO-DOELEN STELLEN</h3>
-          <Collapse isOpened={isOpened}>
+          <Collapse isOpened={isOpened2}>
           
           <div className='vraag'>
             <h>Vraag 2</h>
@@ -149,12 +155,12 @@ class App extends React.Component {
           <input
               className="checkboxinvis"
               type="checkbox"
-              id='checkbox2'
-              checked={isOpened}
-              onChange={({target: {checked}}) => this.setState({isOpened: checked})} />
-              <label for="checkbox2"><img src={collapse} className="imgButton" /></label>
+              id='checkbox3'
+              checked={isOpened3}
+              onChange={({target: {checked}}) => this.setState({isOpened3: checked})} />
+              <label for="checkbox3"><img src={collapse} className="imgButton" /></label>
               <h3>C. MAATREGELEN UITVOEREN</h3>
-            <Collapse isOpened={isOpened}>
+            <Collapse isOpened={isOpened3}>
           
           <label>
             Pick your favorite flavor:
@@ -183,12 +189,12 @@ class App extends React.Component {
           <input
               className="checkboxinvis"
               type="checkbox"
-              id='checkbox2'
-              checked={isOpened}
-              onChange={({target: {checked}}) => this.setState({isOpened: checked})} />
-              <label for="checkbox2"><img src={collapse} className="imgButton" /></label>
+              id='checkbox4'
+              checked={isOpened4}
+              onChange={({target: {checked}}) => this.setState({isOpened4: checked})} />
+              <label for="checkbox4"><img src={collapse} className="imgButton" /></label>
               <h3>D. MONITOREN VAN DE VOORTGANG</h3>
-            <Collapse isOpened={isOpened}>
+            <Collapse isOpened={isOpened4}>
           
           <label>
             Pick your favorite flavor:
@@ -202,14 +208,6 @@ class App extends React.Component {
             <input type="submit" value="Submit" />
           </label>
 
-          <div>
-            <img src = {thermometer} alt="thermometer" width="500" height="400"></img>
-          </div>
-          
-          <div>
-            <img src = {epicLine} alt="Line" width="100" height="10"></img>
-          </div>
-
           </Collapse>
         </div>
 {/* ----------------------------------------------------------------------------------------- */}
@@ -217,12 +215,12 @@ class App extends React.Component {
           <input
               className="checkboxinvis"
               type="checkbox"
-              id='checkbox2'
-              checked={isOpened}
-              onChange={({target: {checked}}) => this.setState({isOpened: checked})} />
-              <label for="checkbox2"><img src={collapse} className="imgButton" /></label>
+              id='checkbox5'
+              checked={isOpened5}
+              onChange={({target: {checked}}) => this.setState({isOpened5: checked})} />
+              <label for="checkbox5"><img src={collapse} className="imgButton" /></label>
               <h3>E. COMMUNICEREN OVER MVO</h3>
-            <Collapse isOpened={isOpened}>
+            <Collapse isOpened={isOpened5}>
           
           <label>
             Pick your favorite flavor:
